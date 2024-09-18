8dd567ba4e630c1bbdc80c3049511077f76679bb
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-09-18 13:43:50 +0100

    Update Favicon and loading gif and remove unecessary files

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { ProgressBar } from 'react-bootstrap';
+import loadingGif from "../../images/benrover_loading.gif";
 
 const Loading = () => {
   return (
-    <div style={styles.container}>
-      <ProgressBar striped animated now={100} min={75} max={100} style={styles.progressBar} />
+    <div>
+      <img src={loadingGif} alt="Loading..." />
     </div>
   );
-};
-
-const styles = {
-    container: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-    },
-    progressBar: {
-      width: '50%',
-      backgroundColor: '#fff',
-      color: '#999696' // Background color for the empty part of the bar
-    },
 };
 
 export default Loading;
