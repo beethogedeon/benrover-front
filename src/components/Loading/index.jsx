3c6ad786e9f3d9abbfb261dce7cb3c2bd4095225
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-09-18 13:58:27 +0100

    Center loading div

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,10 +3,18 @@ import loadingGif from "../../images/benrover_loading.gif";
 
 const Loading = () => {
   return (
-    <div>
+    <div style={containerstyle}>
       <img src={loadingGif} alt="Loading..." />
     </div>
   );
+};
+
+const containerstyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor:'#303130'
 };
 
 export default Loading;
